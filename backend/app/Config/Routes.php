7a9da75976b6568903a3727417224058a6904e4a
IhaveNoIdead <lagumenjrdennis@gmail.com>
2025-10-23 06:02:59 +0800

    frontend (Admin Dashboard) Added Admin Dashboard - added route to Admin Dashboard - added controller for Admin dashboard

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -10,4 +10,5 @@ $routes->get('/moodBoard', 'Users::moodBoard');
 $routes->get('/roadMap', 'Users::roadMap');
 $routes->get('/login', 'Auth::login');
 $routes->get('/signup', 'Auth::signup');
+$routes->get('/admin/dashboard', 'Admin::dashboard');
 
