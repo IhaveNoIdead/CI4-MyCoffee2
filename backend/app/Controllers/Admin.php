88408006d39b6bdc164ab2f0a04fa3ce896175f0
IhaveNoIdead <lagumenjrdennis@gmail.com>
2025-10-30 10:08:24 +0800

    feat: (Admin Accounts Page) added admin accounts page - updated controller Admin.php - updated Routes.php

--- backend/app/Controllers/Admin.php
+++ backend/app/Controllers/Admin.php
@@ -3,11 +3,21 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UsersModel;
 
 class Admin extends BaseController
 {
     public function dashboard()
     {
         return view('admin/dashboard');
+    }
+
+    public function accountsPage()
+    {
+        $usersModel = new UsersModel();
+
+        $data['users'] = $usersModel->findAll();
+        
+        return view('admin/accountsPage', $data);
     }
 }
